46307648e288299e44ec806674fe5dd0adf2b5ba
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-01-07 21:17:30 +0200

    Spiking The App Design & Vue Component: Nav

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="container">
-        Hello world
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav></Nav>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Nav from "./Nav";
+
 export default {
     name: 'App',
+    components: {
+        Nav
+    },
     mounted() {
         console.log('App mounted.')
     }
