65da95a2ca386921e41617fb15e90d18d2162262
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-01-08 08:18:50 +0200

    Fix tailwind not compile just in time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    // These paths are just examples, customize them to match your project structure
+    purge: [
+        './public/**/*.html',
+        './resources/**/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
