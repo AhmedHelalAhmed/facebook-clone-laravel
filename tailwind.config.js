4b0b07e7f335b7fbe9a6a283cd131db4ebc703a1
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-01-08 08:59:00 +0200

    Add NewPost & NewsFeed components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
     mode: 'jit',
     content: [
         "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './resources/js/components/*.{js,vue}',
+        './resources/js/views/*.{js,vue}',
     ],
     // These paths are just examples, customize them to match your project structure
     purge: [
-        './public/**/*.html',
-        './resources/**/**/*.{js,jsx,ts,tsx,vue}',
+        './public/js/*.{html,js}',
+        './public/css/*.css',
     ],
     theme: {
         extend: {},
