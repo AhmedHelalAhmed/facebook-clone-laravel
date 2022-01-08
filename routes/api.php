a0239ced74c2b70892a1770c5fcb9a64a3147863
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-01-08 18:55:37 +0200

    Add user profile test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,14 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
 
-
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::get('/posts', 'PostController@index');
-
-    Route::post('/posts', 'PostController@store');
+    Route::apiResources([
+        'posts' => 'PostController',
+        'users' => 'UserController'
+    ]);
 });
 
 
